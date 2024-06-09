79ba688fe0ec8f359052998c1bb22c988c793bc9
Jakub Bojarowski <qbojarowski@gmail.com>
2024-06-09 11:55:43 +0200

    small ray marching changes

--- Assets/Shaders/RayMarchingController.cs
+++ Assets/Shaders/RayMarchingController.cs
@@ -8,8 +8,12 @@ public class RayMarchingController : MonoBehaviour
     public ComputeShader shader;
     public RenderTexture renderTexture;
     public float cameraZ;
+    public float cameraAngle;
     public Vector2Int resolution;
     public Vector3 repeatVector;
+    public Vector3 repeatVectorOffset;
+    public Vector3 spherePosition;
+    public float timeScale = 1;
 
     void Start()
     {
@@ -21,8 +25,11 @@ public class RayMarchingController : MonoBehaviour
     void Update()
     {
         shader.SetFloat("cameraZ", cameraZ);
-        shader.SetFloat("time", Time.time / 10f);
+        shader.SetFloat("cameraAngle", cameraAngle);
+        shader.SetFloat("time", Time.time * timeScale);
         shader.SetFloats("repeatVector", repeatVector.x, repeatVector.y, repeatVector.z);
+        shader.SetFloats("repeatVectorOffset", repeatVectorOffset.x, repeatVectorOffset.y, repeatVectorOffset.z);
+        shader.SetFloats("spherePosition", spherePosition.x, spherePosition.y, spherePosition.z);
         shader.SetInts("resolution", renderTexture.width, renderTexture.height);
         shader.SetTexture(0, "Result", renderTexture);
         shader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
